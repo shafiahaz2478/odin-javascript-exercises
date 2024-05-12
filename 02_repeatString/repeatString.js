90b6bc7c57c9c048f299e9de901c3e5b1ce1ad55
shafiahaz2478 <69790524+shafiahaz2478@users.noreply.github.com>
2024-05-12 16:55:42 +0530

    Completed escersise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string , times) {
+    let output = "";
+    if(times< 0){
+        return "ERROR";
+    }
+    for(let i = 1; i <= times; i++){
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
