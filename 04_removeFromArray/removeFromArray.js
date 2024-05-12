b48cce5a2e1dfec6c4b77082bd48c2920a9e67fe
shafiahaz2478 <69790524+shafiahaz2478@users.noreply.github.com>
2024-05-12 20:02:29 +0530

    completed 4th

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,31 @@
 const removeFromArray = function() {
+    let output = [];
+    let arg = arguments[0];
+    console.log("start: " +arg);
+    for(let i = 1 ; i < arguments.length;i++){
+        let value = arguments[i];
+        if(!(typeof value === "string")){ 
+            arg.splice(value - 1, 1);
+        }
+        
+    }
 
-};
+    for (let i = 1; i < arguments.length; i++) {
+        const value = arguments[i];
+        for(let j = 0; j < arg.length;j++){
+            if(arg[j] === value){
+                
+                arg.splice(j , 1);
+                j--;
+                
+
+            }
+        }
+        
+    }
+    console.log(arg);
+    return arg;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
