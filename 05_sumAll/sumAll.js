bf91143ee207bb3007a10e419f751471426e3758
shafiahaz2478 <69790524+shafiahaz2478@users.noreply.github.com>
2024-05-12 20:09:17 +0530

    completed 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(intial, final) {
+    let sum = 0;
+    if(intial < 0 || final < 0 || typeof intial !== "number" || typeof final !== "number"){
+        return "ERROR";
+    }
 
+    if(intial > final){
+        let i = final;
+        final = intial;
+        intial = i;
+    }
+    while(intial <= final){
+        sum += intial;
+        intial++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
