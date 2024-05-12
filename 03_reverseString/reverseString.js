863cdb7d8f56596bfebbf8cb959b6a414605d6b4
shafiahaz2478 <69790524+shafiahaz2478@users.noreply.github.com>
2024-05-12 17:02:09 +0530

    completed 3rd escersice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let output = "";
+    for(let i = 0 ; i < string.length;i++){
+        output = string.charAt(i) + output;
+    }
 
+    return output;
 };
 
 // Do not edit below this line
